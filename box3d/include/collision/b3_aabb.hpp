baa8256608e55d825af79c4e5f57f9dd77b8c836
burningbabyq <2942465016@qq.com>
2024-07-10 20:21:58 +0800

    add annotation to aabb

--- box3d/include/collision/b3_aabb.hpp
+++ box3d/include/collision/b3_aabb.hpp
@@ -1,10 +1,38 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #ifndef BOX3D_B3_AABB_HPP
 #define BOX3D_B3_AABB_HPP
 
 #include "common/b3_types.hpp"
 #include "math/b3_vec3.hpp"
-
+/**
+ * @brief The b3AABB class represents an Axis-Aligned Bounding Box (AABB),
+ * used to define a box in three-dimensional space with minimum and maximum boundaries,
+ * and provides related operations.
+ */
 class b3AABB {
 
     friend class b3CubeShape;
@@ -13,19 +41,28 @@ class b3AABB {
     friend class b3DynamicTree;
 
     /**
-     * The lower bound of the AABB.
+     * @brief The lower bound of the AABB.
      */
     b3Vec3r m_min;
 
     /**
-     * The upper bound of the AABB.
+     * @brief The upper bound of the AABB.
      */
     b3Vec3r m_max;
 
 public:
 
+    /**
+     * @brief The default constructor of b3AABB.
+     */
     b3AABB() = default;
 
+    /**
+     * @brief constructor of b3AABB
+     * @param lower_bound lower bound of the b3AABB
+     * @param upper_bound upper bound of the b3AABB
+     * @attention The lower bound must be less than the upper bound.
+     */
     b3AABB(const b3Vec3r& lower_bound, const b3Vec3r& upper_bound);
 
     /**
@@ -37,10 +74,18 @@ public:
         return real(0.5) * (m_max - m_min);
     }
 
+    /**
+     * @brief Get the minimum coordinate of the AABB.
+     * @return The minimum coordinate of the AABB.
+     */
     inline b3Vec3r min() const {
         return m_min;
     }
 
+    /**
+     * @brief Get the maximum coordinate of the AABB.
+     * @return The maximum coordinate of the AABB.
+     */
     inline b3Vec3r max() const {
         return m_max;
     }
@@ -59,6 +104,8 @@ public:
 
     /**
      * @brief  Combine 2 AABBs into 1 AABB.
+     * @param A one of the AABBs
+     * @param B one of the AABBs
      */
     inline void combine(const b3AABB& A, const b3AABB& B) {
 
@@ -68,6 +115,9 @@ public:
 
     /**
      * @brief Check this AABB contains another AABB.
+     * @param other potentially contained AABB.
+     * @return Returns true if this AABB completely contains the other AABB.
+     * Returns false if it partially contains or does not contain the other AABB at all.
      */
     inline bool contains(const b3AABB& other) const {
 
--- box3d/src/collision/b3_aabb.cpp
+++ box3d/src/collision/b3_aabb.cpp
@@ -1,13 +1,48 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #include "collision/b3_aabb.hpp"
 
-
+/**
+ * @brief constructor of b3AABB
+ * @param lower_bound lower bound of the b3AABB
+ * @param upper_bound upper bound of the b3AABB
+ * @attention The lower bound must be less than the upper bound.
+ */
 b3AABB::b3AABB(const b3Vec3r &lower_bound, const b3Vec3r &upper_bound) {
     m_min = lower_bound;
     m_max = upper_bound;
 }
 
-
+/**
+ * @brief Check whether 2 AABBs are overlapped.
+ * @param A: The first AABB.
+ * @param B: The second AABB.
+ * @details This method is introduced in @cite Real-Time Collision Detection, Chapter 4
+ * @return True if 2 AABBs are overlapped, false otherwise.
+ */
 bool b3AABB::overlapped(const b3AABB &A, const b3AABB &B)
 {
     if (A.m_max.x < B.m_min.x || A.m_min.x > B.m_max.x) return false;
