b1bdea4e0c350a2207e690f92cf2dfef0c49d7de
burningbabyq <2942465016@qq.com>
2024-07-11 12:17:47 +0800

    add annotation to collision.hpp & collide.cpp

--- box3d/include/collision/b3_collision.hpp
+++ box3d/include/collision/b3_collision.hpp
@@ -1,3 +1,28 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
 
 #ifndef BOX3D_B3_COLLISION_HPP
 #define BOX3D_B3_COLLISION_HPP
@@ -6,7 +31,7 @@
 #include "dynamics/b3_transform.hpp"
 
 
-/////////// Forward Delaration ///////////
+/////////// Forward Declaration ///////////
 
 class b3CubeShape;
 
@@ -17,60 +42,122 @@ class b3PlaneShape;
 //////////////////////////////////////////
 
 
-/// The features that intersect to form the contact point
-/// This must be 4 bytes or less.
+/**
+ * @brief The features that intersect to form the contact point
+ * This must be 4 bytes or less.
+ */
 struct b3ContactFeature
 {
+
+    /**
+     * @brief enum-type
+     * e_f_p: corresponds to the hexadecimal value 0x01, indicating the face-point type
+     * e_e_e: corresponds to the hexadecimal value 0x02, indicating the edge-edge type.
+     * e_p_f: corresponds to the hexadecimal value 0x04, indicating the point-faces type
+     */
     enum Type {
         e_f_p = 0x01,
         e_e_e = 0x02,
         e_p_f = 0x04
     };
 
+    /**
+     * @brief Represents the type of a feature
+     */
     uint8 type;
 
+    /**
+     * @brief Contact feature related index
+     */
     uint8 index_1;
 
+    /**
+     * @brief Contact feature related index
+     */
     uint8 index_2;
 
+    /**
+     * @brief Contact feature related index, indicates extended index or additional index information
+     */
     uint8 index_ext;
 
 };
 
-
+/**
+ * @brief b3ContactID is a union of b3ContactFeature and uint32 key,
+ * used for fast comparison of contact identifiers.
+ */
 union b3ContactID {
 
+    /**
+     * @brief Represents detailed information about contact features
+     */
     b3ContactFeature cf;
 
-    uint32 key;					///< Used to quickly compare contact ids.
+    /**
+    * @brief Used to quickly compare contact ids.
+    */
+    uint32 key;
 
 };
 
-
+/**
+ * @brief Represents the clipping vertex in collision detection
+ */
 struct b3ClipVertex {
 
+    /**
+     * @brief v: a vertex
+     */
     b3Vec3r v;
 
+    /**
+     * @brief The contact identifier associated with the vertex
+     */
     b3ContactID id;
 
 };
 
 
 // TODO: check to delete this struct.
+/**
+ * @brief Describe the point of contact between two shapes
+ */
 struct b3ManifoldPoint {
 
-    b3Vec3r m_local_point;		///< usage depends on manifold type
+    /**
+     * @brief usage depends on manifold type
+     */
+    b3Vec3r m_local_point;
 
-    float m_normal_impulse;	///< the non-penetration impulse
+    /**
+     * @brief the non-penetration impulse
+     */
+    float m_normal_impulse;
 
-    float m_tangent_impulse;	///< the friction impulse
+    /**
+     * @brief the friction impulse
+     */
+    float m_tangent_impulse;
 
-    b3ContactID id;			///< uniquely identifies a contact point between two shapes
+    /**
+     * @brief uniquely identifies a contact point between two shapes
+     */
+    b3ContactID id;
 };
 
-
+/**
+ * @brief Represents a collision manifold that stores contact points
+ */
 struct b3Manifold {
 
+    /**
+     * @brief enum-type
+     * e_spheres: indicates collisions between two spheres
+     * e_face_A: indicates A face A in the collision.
+     * e_face_B: indicates the other side B in the collision.
+     * e_edges: Edges in a collision.
+     */
     enum Type {
 
         e_spheres,
@@ -82,59 +169,134 @@ struct b3Manifold {
         e_edges
     };
 
-    b3ManifoldPoint m_points[8];	///< the points of contact
+    /**
+     * @brief the points of contact
+     */
+    b3ManifoldPoint m_points[8];
 
-    b3Vec3r m_local_normal;								///< not use for Type::e_points
+    /**
+     * @brief not use for Type::e_points
+     */
+    b3Vec3r m_local_normal;
 
-    b3Vec3r m_local_point;								///< usage depends on manifold type
+    /**
+     * @brief usage depends on manifold type
+     */
+    b3Vec3r m_local_point;
 
+    /**
+     * @brief enum-type
+     */
     Type m_type;
 
-    int32 m_point_count;								///< the number of manifold points
+    /**
+     * @brief  the number of manifold points
+     */
+    int32 m_point_count;
 };
 
-
+/**
+ * @brief Calculate and store collision information in world space
+ */
 struct b3WorldManifold {
 
+    /**
+     * @brief Initialize the member variables of the b3WorldManifold structure based on
+     * the given collision manifold and the transformation information of two shapes.
+     * @param manifold Contact point set
+     * @param xf_A The transformation of shape A
+     * @param radius_A the radius of A
+     * @param xf_B The transformation of shape B
+     * @param radius_B the radius of B
+     */
     void initialize(const b3Manifold* manifold,
                     const b3Transr& xf_A, real radius_A,
                     const b3Transr& xf_B, real radius_B);
 
+    /**
+     * @brief Collision surface normal
+     */
     b3Vec3r normal;
+
+    /**
+     * @brief Groups of contact points in world space
+     */
     b3Vec3r points[8];
+
+    /**
+     * @brief Contact point distance/separation distance array
+     */
     real separations[8];
 };
 
-
-/// Compute the collision manifold between two circles.
+/**
+ * @brief Compute the collision manifold between two circles.
+ * @param manifold A pointer to the b3Manifold structure that will be filled with collision details if the spheres collide
+ * @param sphere_a A pointer to the shape of the first sphere involved in the collision
+ * @param xf_a Constant reference, transformation of the first sphere (position and direction)
+ * @param sphere_b A pointer to the shape of the second sphere involved in the collision
+ * @param xf_b Transformation of the second sphere (position and direction)
+ */
 void b3_collide_spheres(b3Manifold* manifold,
 					    const b3SphereShape* sphere_a,
                         const b3Transr& xf_a,
 					    const b3SphereShape* sphere_b,
                         const b3Transr& xf_b);
 
-/// Compute the collision manifold between circle and cube
+
+/**
+ * @brief Compute the collision manifold between circle and cube
+ * @param manifold If the cube and the sphere collide, the structure will be filled with collision details.
+ * @param cube_a A pointer to the cube shape involved in the collision
+ * @param xf_a Transformation of the cube (position and orientation)
+ * @param sphere_b A pointer to the shape of the sphere involved in the collision
+ * @param xf_b Transformation of the sphere (position and direction)
+ */
 void b3_collide_cube_and_sphere(b3Manifold* manifold,
                                 const b3CubeShape* cube_a,
                                 const b3Transr& xf_a,
                                 const b3SphereShape* sphere_b,
                                 const b3Transr& xf_b);
 
-/// Compute the collision manifold between two cubes
+
+/**
+ * @brief Compute the collision manifold between two cubes
+ * @param manifold Pointer to the b3Manifold structure for storing collision details between cubes
+ * @param cube_A Pointer to the first cube shape involved in the collision
+ * @param xf_A The transformation information of the first cube, including position and orientation
+ * @param cube_B Pointer to the second cube shape involved in the collision
+ * @param xf_B Transformation information for the second cube, including position and orientation.
+ */
 void b3_collide_cube(b3Manifold* manifold,
                      const b3CubeShape* cube_A,
                      const b3Transr& xf_A,
                      const b3CubeShape* cube_B,
                      const b3Transr& xf_B);
 
-/// Compute the collision manifold between a plane and a sphere.
+
+/**
+ * @brief Compute the collision manifold between a plane and a sphere.
+ * @param manifold Pointer to the b3Manifold structure for storing details of collisions between the plane and the sphere
+ * @param plane_a A pointer to the shape of the plane involved in the collision
+ * @param xf_a Information about the transformation of the plane, including position and direction
+ * @param sphere_b A pointer to the shape of the sphere involved in the collision
+ * @param xf_b Information about the transformation of the sphere, including position and direction
+ */
 void b3_collide_plane_and_sphere(b3Manifold* manifold,
                                  const b3PlaneShape* plane_a,
                                  const b3Transr& xf_a,
                                  const b3SphereShape* sphere_b,
                                  const b3Transr& xf_b);
 
-/// Compute the collision manifold between a plane and a cube.
+
+/**
+ * @brief Compute the collision manifold between a plane and a cube.
+ * @param manifold Pointer to the b3Manifold structure for storing details of collisions between planes and cubes
+ * @param plane_a A pointer to the shape of the plane involved in the collision
+ * @param xf_a Information about the transformation of the plane, including position and direction
+ * @param cube_b A pointer to the cube shape involved in the collision
+ * @param xf_b The transformation information of the cube, including position and orientation
+ */
 void b3_collide_plane_and_cube(b3Manifold* manifold,
                                const b3PlaneShape* plane_a,
                                const b3Transr& xf_a,
--- box3d/src/collision/b3_collide.cpp
+++ box3d/src/collision/b3_collide.cpp
@@ -1,7 +1,39 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #include "collision/b3_collision.hpp"
 
-
+/**
+ * @brief Initialize the member variables of the b3WorldManifold structure based on
+ * the given collision manifold and the transformation information of two shapes.
+ * @param manifold Contact point set
+ * @param xf_A The transformation of shape A
+ * @param radius_A the radius of A
+ * @param xf_B The transformation of shape B
+ * @param radius_B the radius of B
+ */
 void b3WorldManifold::initialize(
         const b3Manifold *manifold,
         const b3Transr &xf_A, real radius_A,
