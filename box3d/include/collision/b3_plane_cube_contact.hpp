81c08e09f125c7ac3d6a8a6d20a95ce986e1e4f7
burningbabyq <2942465016@qq.com>
2024-07-14 12:24:13 +0800

    add annotation to plane_cube_contact.hpp&cpp

--- box3d/include/collision/b3_plane_cube_contact.hpp
+++ box3d/include/collision/b3_plane_cube_contact.hpp
@@ -5,19 +5,50 @@
 
 #include "collision/b3_contact.hpp"
 
-
+/**
+ * @brief The b3PlaneCubeContact class represents a specific type of contact
+ * between a plane and a cube in a physics simulation.
+ */
 class b3PlaneCubeContact: public b3Contact {
 
 public:
 
+    /**
+     * @brief Static method to create a b3PlaneCubeContact object given two fixtures and a block allocator
+     * @param fixture_a Pointer to the first fixture (plane).
+     * @param index_a Index related to fixture A
+     * @param fixture_b Pointer to the second fixture (cube).
+     * @param index_b Index related to fixture B
+     * @param block_allocator Pointer to the block allocator for memory management.
+     * @return Returns a pointer to the newly created b3PlaneCubeContact object.
+     */
     static b3Contact* create(b3Fixture* fixture_a, int32 index_a, b3Fixture* fixture_b, int32 index_b, b3BlockAllocator* block_allocator);
 
+    /**
+     * @brief Static method to destroy a b3PlaneCubeContact object and free its memory using a block allocator.
+     * @param contact Pointer to the contact object to destroy.
+     * @param block_allocator Pointer to the block allocator for memory management.
+     */
     static void destroy(b3Contact* contact, b3BlockAllocator* block_allocator);
 
+    /**
+     * @brief  Constructor that initializes the contact with two fixtures (a plane and a cube).
+     * @param fixture_a Pointer to the first fixture (plane).
+     * @param fixture_b Pointer to the second fixture (cube).
+     */
     b3PlaneCubeContact(b3Fixture* fixture_a, b3Fixture* fixture_b);
 
+    /**
+     * @brief Virtual destructor to properly destroy instances of b3PlaneCubeContact.
+     */
     virtual ~b3PlaneCubeContact() = default;
 
+    /**
+     * @brief Method to evaluate and compute the collision manifold (contact points) between the plane and cube.
+     * @param manifold Pointer to the manifold structure where the collision information is stored.
+     * @param xf_a Transform of the first fixture (plane).
+     * @param xf_b Transform of the second fixture (cube).
+     */
     void evaluate(b3Manifold* manifold, const b3Transr& xf_a, const b3Transr& xf_b) override;
 
 };
--- box3d/src/collision/b3_plane_cube_contact.cpp
+++ box3d/src/collision/b3_plane_cube_contact.cpp
@@ -2,7 +2,15 @@
 #include "collision/b3_fixture.hpp"
 #include "collision/b3_collision.hpp"
 
-
+/**
+ * @brief Static method to create a b3PlaneCubeContact object given two fixtures and a block allocator
+ * @param fixture_a Pointer to the first fixture (plane).
+ * @param index_a Index related to fixture A
+ * @param fixture_b Pointer to the second fixture (cube).
+ * @param index_b Index related to fixture B
+ * @param block_allocator Pointer to the block allocator for memory management.
+ * @return Returns a pointer to the newly created b3PlaneCubeContact object.
+ */
 b3Contact* b3PlaneCubeContact::create(
     b3Fixture *fixture_a, int32 index_a,
     b3Fixture *fixture_b, int32 index_b,
@@ -12,14 +20,22 @@ b3Contact* b3PlaneCubeContact::create(
     return new (mem) b3PlaneCubeContact(fixture_a, fixture_b);
 }
 
-
+/**
+ * @brief Static method to destroy a b3PlaneCubeContact object and free its memory using a block allocator.
+ * @param contact Pointer to the contact object to destroy.
+ * @param block_allocator Pointer to the block allocator for memory management.
+ */
 void b3PlaneCubeContact::destroy(b3Contact *contact, b3BlockAllocator *block_allocator)
 {
     ((b3PlaneCubeContact*)contact)->~b3PlaneCubeContact();
     block_allocator->free(contact, sizeof(b3PlaneCubeContact));
 }
 
-
+/**
+ * @brief  Constructor that initializes the contact with two fixtures (a plane and a cube).
+ * @param fixture_a Pointer to the first fixture (plane).
+ * @param fixture_b Pointer to the second fixture (cube).
+ */
 b3PlaneCubeContact::b3PlaneCubeContact(b3Fixture *fixture_a, b3Fixture *fixture_b):
     b3Contact(fixture_a, 0, fixture_b, 0)
 {
@@ -27,7 +43,12 @@ b3PlaneCubeContact::b3PlaneCubeContact(b3Fixture *fixture_a, b3Fixture *fixture_
     b3_assert(fixture_b->get_shape_type() == e_cube);
 }
 
-
+/**
+ * @brief Method to evaluate and compute the collision manifold (contact points) between the plane and cube.
+ * @param manifold Pointer to the manifold structure where the collision information is stored.
+ * @param xf_a Transform of the first fixture (plane).
+ * @param xf_b Transform of the second fixture (cube).
+ */
 void b3PlaneCubeContact::evaluate(b3Manifold *manifold, const b3Transr &xf_a, const b3Transr &xf_b)
 {
     b3_collide_plane_and_cube(
