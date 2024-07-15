3f467b1f36f341829705e5e8c30bb8ebb7ba3de8
burningbabyq <2942465016@qq.com>
2024-07-15 19:46:29 +0800

    add annotation to island.hpp/cpp

--- box3d/include/dynamics/b3_island.hpp
+++ box3d/include/dynamics/b3_island.hpp
@@ -1,3 +1,27 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #ifndef BOX3D_B3_ISLAND_HPP
 #define BOX3D_B3_ISLAND_HPP
@@ -6,7 +30,7 @@
 #include "common/b3_types.hpp"
 
 
-/////////// Forward Delaration ///////////
+/////////// Forward Declaration ///////////
 
 class b3Contact;
 
@@ -16,51 +40,116 @@ class b3BlockAllocator;
 
 //////////////////////////////////////////
 
-
+/**
+ * @brief The b3Island class is designed to manage a group of b3Body and b3Contact
+ * objects during a simulation step in a physics engine.
+ */
 class b3Island {
 
+    /**
+     * @brief Tracks the current number of bodies in the island.
+     */
     int32 m_body_count;
+    /**
+     * @brief Tracks the current number of contacts in the island.
+     */
 	int32 m_contact_count;
 
+    /**
+     * @brief Sets the maximum number of bodies the island can manage.
+     */
 	int32 m_body_capacity;
+    /**
+     * @brief Sets the maximum number of contacts the island can manage.
+     */
 	int32 m_contact_capacity;
 
+    /**
+     * @brief Holds pointers to the bodies within the island.
+     */
     b3Body** m_bodies;
+    /**
+     * @brief Holds pointers to the contacts within the island.
+     */
     b3Contact** m_contacts;
 
+    /**
+     * @brief Manages memory allocation for the island.
+     */
     b3BlockAllocator* m_block_allocator;
 
 public:
 
+    /**
+     * @brief the constructor of b3Island class
+     * @param block_allocator Allocator used for memory management.
+     * @param body_capacity Maximum number of bodies the island can manage.
+     * @param contact_capacity Maximum number of contacts the island can manage.
+     */
     b3Island(b3BlockAllocator* block_allocator, int32 body_capacity, int32 contact_capacity);
 
+    /**
+     * @brief the destructor of b3Island class
+     */
     ~b3Island();
 
+    /**
+     * @brief Adds a body to the island.
+     * @param body Pointer to the body to be added.
+     */
     void add_body(b3Body* body);
 
+    /**
+     * @brief Adds a contact to the island.
+     * @param contact Pointer to the contact to be added.
+     */
     void add_contact(b3Contact* contact);
 
+    /**
+     * @brief Resets the island by clearing the count of bodies and contacts.
+     */
     void clear() {
         m_body_count = 0;
         m_contact_count = 0;
     }
 
+    /**
+     * @brief Retrieves the array of contacts.
+     * @return returns the array of contacts.
+     */
     b3Contact** get_contacts() const {
         return m_contacts;
     }
 
+    /**
+     * @brief Gets the number of contacts in the island.
+     * @return returns the number of contacts in the island.
+     */
     int get_contacts_count() const {
         return m_contact_count;
     }
 
+    /**
+     * @brief Gets the number of bodies in the island.
+     * @return returns the number of bodies in the island.
+     */
     int get_body_count() const {
         return m_body_count;
     }
 
+    /**
+     * @brief Retrieves the array of bodies.
+     * @return returns the array of bodies.
+     */
     b3Body** get_bodies() const {
         return m_bodies;
     }
 
+    /**
+     * @brief Retrieves a specific body by index.
+     * @param index Index of the body to retrieve.
+     * @return Pointer to the b3Body at the specified index.
+     */
     b3Body* get_body(int32 index) {
         return m_bodies[index];
     }
--- box3d/src/dynamics/b3_island.cpp
+++ box3d/src/dynamics/b3_island.cpp
@@ -1,3 +1,27 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #include "dynamics/b3_island.hpp"
 #include "dynamics/b3_body.hpp"
@@ -5,7 +29,12 @@
 
 #include "common/b3_block_allocator.hpp"
 
-
+/**
+ * @brief the constructor of b3Island class
+ * @param block_allocator Allocator used for memory management.
+ * @param body_capacity Maximum number of bodies the island can manage.
+ * @param contact_capacity Maximum number of contacts the island can manage.
+ */
 b3Island::b3Island(b3BlockAllocator* block_allocator, int32 body_capacity, int32 contact_capacity):
     m_body_capacity(body_capacity), m_contact_capacity(contact_capacity)
 {
@@ -20,7 +49,10 @@ b3Island::b3Island(b3BlockAllocator* block_allocator, int32 body_capacity, int32
     m_contacts = new (mem) b3Contact*;
 }
 
-
+/**
+ * @brief Adds a body to the island.
+ * @param body Pointer to the body to be added.
+ */
 void b3Island::add_body(b3Body* body)
 {
     b3_assert(m_body_count < m_body_capacity);
@@ -30,7 +62,10 @@ void b3Island::add_body(b3Body* body)
     m_body_count++;
 }
 
-
+/**
+ * @brief Adds a contact to the island.
+ * @param contact Pointer to the contact to be added.
+ */
 void b3Island::add_contact(b3Contact* contact)
 {
     b3_assert(m_contact_count < m_contact_capacity);
@@ -39,7 +74,9 @@ void b3Island::add_contact(b3Contact* contact)
     m_contact_count++;
 }
 
-
+/**
+ * @brief the destructor of b3Island class
+ */
 b3Island::~b3Island()
 {
     m_block_allocator->free(m_bodies, m_body_capacity * sizeof(b3Body*));
