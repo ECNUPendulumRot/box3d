7b4259053f19e53126d43a2297d072cdfa4e13f4
burningbabyq <2942465016@qq.com>
2024-07-15 21:28:26 +0800

    add annotation to plane_shape.hpp/cpp

--- box3d/include/geometry/b3_plane_shape.hpp
+++ box3d/include/geometry/b3_plane_shape.hpp
@@ -1,3 +1,27 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #ifndef BOX3D_B3PLANESHAPE_HPP
 #define BOX3D_B3PLANESHAPE_HPP
@@ -5,31 +29,64 @@
 
 #include "geometry/b3_shape.hpp"
 
-
+/**
+ * @brief represents a plane shape in a 3D physics engine.
+ */
 class b3PlaneShape : public b3Shape {
 
 public:
 
+    /**
+     * @brief m_half_length: Represents half of the plane's length.
+     * m_half_width: Represents half of the plane's width.
+     */
     real m_half_length, m_half_width;
 
+    /**
+     * @brief Stores the four vertices of the plane in 3D space.
+     */
     b3Vec3r m_vertices[4];
 
+    /**
+     * @brief the constructor of b3PlaneShape
+     */
     b3PlaneShape();
 
     /**
+     * @brief Defines the plane's dimensions and calculates its vertices.
      * @param point: a point on this plane
      * @param normal: the normal of this plane
     */
     void set_as_plane(real length, real width);
 
+    /**
+     * @brief Returns the number of child shapes. For a plane, this is always 1.
+     * @return
+     */
     int32 get_child_count() const {
         return 1;
     }
 
+    /**
+     * @brief Computes the axis-aligned bounding box (AABB) for the plane.
+     * @param aabb Pointer to the AABB to be computed.
+     * @param xf Transformation applied to the plane.
+     * @param child_index Index of the child shape.
+     */
     void get_bound_aabb(b3AABB* aabb, const b3Transr& xf, int32 child_index) const override;
 
+    /**
+     * @brief Calculates the mass properties of the plane. Since a plane is a static object,
+     * its mass and inertia are zero.
+     * @param mass_data Reference to the mass properties to be computed.
+     * @param density Density of the plane.
+     */
     void compute_mass_properties(b3MassProperty& mass_data, real density) const override;
 
+    /**
+     * @brief Creates and returns a deep copy of the current b3PlaneShape object.
+     * @return A pointer to the newly created clone of the b3PlaneShape object.
+     */
     b3Shape* clone() const override;
 };
 
--- box3d/src/geometry/b3_plane_shape.cpp
+++ box3d/src/geometry/b3_plane_shape.cpp
@@ -1,3 +1,26 @@
+// The MIT License
+
+// Copyright (c) 2024
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #include "geometry/b3_plane_shape.hpp"
 #include "common/b3_block_allocator.hpp"
@@ -7,14 +30,20 @@
 
 #include "collision/b3_aabb.hpp"
 
-
+/**
+ * @brief the constructor of b3PlaneShape
+ */
 b3PlaneShape::b3PlaneShape()
 {
     m_radius = b3_polygon_radius;
     m_type = e_plane;
 }
 
-
+/**
+ * @brief Defines the plane's dimensions and calculates its vertices.
+ * @param point: a point on this plane
+ * @param normal: the normal of this plane
+*/
 void b3PlaneShape::set_as_plane(real length, real width)
 {
     m_half_length = length / 2.0;
@@ -25,7 +54,12 @@ void b3PlaneShape::set_as_plane(real length, real width)
     m_vertices[3].set(m_half_width, -m_half_length, 0);
 }
 
-
+/**
+ * @brief Computes the axis-aligned bounding box (AABB) for the plane.
+ * @param aabb Pointer to the AABB to be computed.
+ * @param xf Transformation applied to the plane.
+ * @param child_index Index of the child shape.
+ */
 void b3PlaneShape::get_bound_aabb(b3AABB* aabb, const b3Transr& xf, int32 child_index) const
 {
     b3_NOT_USED(child_index);
@@ -51,7 +85,12 @@ void b3PlaneShape::get_bound_aabb(b3AABB* aabb, const b3Transr& xf, int32 child_
     aabb->m_max = max + r;
 }
 
-
+/**
+ * @brief Calculates the mass properties of the plane. Since a plane is a static object,
+ * its mass and inertia are zero.
+ * @param mass_data Reference to the mass properties to be computed.
+ * @param density Density of the plane.
+ */
 void b3PlaneShape::compute_mass_properties(b3MassProperty &mass_data, real density) const
 {
     // plane is static object
@@ -61,6 +100,10 @@ void b3PlaneShape::compute_mass_properties(b3MassProperty &mass_data, real densi
     mass_data.m_Inertia = b3Mat33r::zero();
 }
 
+/**
+ * @brief Creates and returns a deep copy of the current b3PlaneShape object.
+ * @return A pointer to the newly created clone of the b3PlaneShape object.
+ */
 b3Shape* b3PlaneShape::clone() const
 {
     void* mem = m_block_allocator->allocate(sizeof(b3PlaneShape));
